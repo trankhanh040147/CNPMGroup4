a967634fab19c3785004594b7d8817468443c642
An Ngo <93450617+AnNgoIT@users.noreply.github.com>
2022-11-25 23:40:23 +0700

    file sql
    the second fix, add data, constraint on database?

--- QuanLyDeTai.sql
+++ QuanLyDeTai.sql
@@ -36,13 +36,13 @@ IF NOT EXISTS
 (
     SELECT [name]
     FROM sys.databases
-    WHERE [name] = N'QUANLYCUAHANG'
+    WHERE [name] = N'QUANLYDETAI'
 )
-    CREATE DATABASE QUANLYCUAHANG;
+    CREATE DATABASE QUANLYDETAI;
 
 GO
 
-USE QUANLYCUAHANG;
+USE QUANLYDETAI;
 
 -- Create a new table called '[TaiKhoan]' in schema '[dbo]'
 
@@ -67,7 +67,6 @@ CREATE TABLE [dbo].[TaiKhoan]
 
 GO
 
-
 -- Create a new table called '[HoiDongPB]' in schema '[dbo]'
 
 -- Drop the table if it already exists
@@ -89,7 +88,6 @@ CREATE TABLE [dbo].[HoiDongPB]
 
 GO
 
-
 -- Create a new table called '[DotDangKy]' in schema '[dbo]'
 
 -- Drop the table if it already exists
@@ -110,7 +108,6 @@ CREATE TABLE [dbo].[DotDangKy]
 );
 
 GO
-
 
 -- Create a new table called '[DeTai]' in schema '[dbo]'
 
@@ -135,7 +132,6 @@ CREATE TABLE [dbo].[DeTai]
 	MaGVHD varchar(10) references dbo.TaiKhoan(MaTaiKhoan),
 	MaGVPB varchar(10) references dbo.TaiKhoan(MaTaiKhoan),
 	DuocDuyet BIT NOT NULL DEFAULT 0, --Mặc định là chưa được duyệt
-
 );
 
 GO
@@ -156,7 +152,6 @@ CREATE TABLE [dbo].[NhomSinhVien]
 (
     MaNhom varchar(25) primary key, 
 	MaNhomtruong varchar(10) references dbo.TaiKhoan(MaTaiKhoan),
-	
 	maDeTai varchar(25) references dbo.DeTai(maDeTai),
 	SoThanhVien int not null,
 );
@@ -183,6 +178,48 @@ CREATE TABLE [dbo].[ThanhVienNhom]
 
 GO
 
+insert into dbo.TaiKhoan values('20110123', N'Nguyễn Hoàng Lan', 'hoanglan2k2@mail.com', '0978978335','','20110123', 3)
+insert into dbo.TaiKhoan values('20110279', N'Lê Hoài Lâm', 'lehoailam@gmail.com', '0356536245','','20110279', 3)
+insert into dbo.TaiKhoan values('20110356', N'Trần Văn Khánh', 'tvkhanh@gmail.com', '0256536245','','20110356', 3)
+insert into dbo.TaiKhoan values('20110004', N'Lê Hoài Đức', 'ducleh@gmail.com', '0356534777','','20110004', 3)
+insert into dbo.TaiKhoan values('20110009', N'Ngô Thanh Ân', 'thanhanngo@gmail.com', '0976978204','','20110009', 3)
+insert into dbo.TaiKhoan values('20110089', N'Nguyễn Tuyết Lan', 'tuyetlan@gmail.com', '0836978204','','20110089', 3)
+insert into dbo.TaiKhoan values('20110247', N'Hoàng Văn Thái', 'thaihv@gmail.com', '0126978204','','20110247', 3)
+insert into dbo.TaiKhoan values('GVCNTT0054', N'Quách Đình Huỳnh', 'huynhdq@gmail.com', '0967278204','','GVCNTT0054', 2)
+insert into dbo.TaiKhoan values('GVCNTT0126', N'Trần Văn Đức', 'ductv@mail.com', '0237777838','','GVCNTT0126', 2)
+insert into dbo.TaiKhoan values('GVCNTT0252', N'Nguyễn Hữu Nghĩa', 'nghianh@gmail.com', '0936532786','','GVCNTT0252', 2)
+insert into dbo.TaiKhoan values('TBMCNPM012', N'Huỳnh Xuân Phụng', 'phunghx@gmail.com', '0236723596','','TBMCNPM012', 1)
+insert into dbo.TaiKhoan values('TBMHTTT014', N'Nguyễn Thành Sơn', 'sonnt@gmail.com', '0935932786','','TBMHTTT014', 1)
 
+GO
+
+insert into dbo.DeTai values('DT_CNPM_TLCN_2122_001', N'Xây dựng ứng dụng mạng xã hội sản phẩm trên Android', N'Công nghệ phần mềm', '', 1, NULL, NULL, NULL, NULL, 0)
+insert into dbo.DeTai values('DT_HTTT_TLCN_2122_034', N'Xây dựng ứng dụng IoT với NodeJS.', N'Hệ thống thông tin', 'Đã thi đậu các môn: Cơ sở dữ liệu, Hệ quản trị cơ sở dữ liệu', 1, NULL, NULL, NULL, NULL, 0)
+
+GO
+
+insert into dbo.NhomSinhVien values('NH_CNPM_TLCN_2122_009', '20110279', NULL, 2)
+insert into dbo.NhomSinhVien values('NH_HTTT_TLCN_2122_015', '20110123', NULL, 1)
+
+GO
+
+insert into dbo.ThanhVienNhom values('NH_CNPM_TLCN_2122_009', '20110279')
+insert into dbo.ThanhVienNhom values('NH_CNPM_TLCN_2122_009', '20110089')
+insert into dbo.ThanhVienNhom values('NH_HTTT_TLCN_2122_015', '20110123')
+
+GO
+
+insert into dbo.HoiDongPB values('HDPB_CNPM_TLCN_2122_001', NULL, NULL, 5)
+insert into dbo.HoiDongPB values('HDPB_CNPM_TLCN_2122_002', NULL, NULL, 5)
+insert into dbo.HoiDongPB values('HDPB_HTTT_TLCN_2122_001', NULL, NULL, 6)
+
+GO
+
+insert into dbo.DotDangKy values('DK_TLCN_2122_D1', 1, '2021-08-07', '2022-02-07')
+insert into dbo.DotDangKy values('DK_TLCN_2122_D2', 2, '2022-02-10', NULL)
+insert into dbo.DotDangKy values('DK_KLTN_2122_D1', 1, '2021-06-15', '2022-03-12')
+insert into dbo.DotDangKy values('DK_KLTN_2122_D2', 2, '2022-01-21', NULL)
+
+GO
 
 
