ffc17679eb1b4c46998195a7ca1200ba6c5656b8
trankhanh040147 <78889860+trankhanh040147@users.noreply.github.com>
2022-11-28 20:31:30 +0700

    View Home (guest), view Login

--- ProjectGroup4/src/main/java/vn/dkdtute/Controller/HomeController.java
+++ ProjectGroup4/src/main/java/vn/dkdtute/Controller/HomeController.java
@@ -12,15 +12,21 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HomeController
  */
-@WebServlet(urlPatterns = { "/HomeController" })
+@WebServlet(urlPatterns = { "/home"})
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		RequestDispatcher dispatcherr = request.getRequestDispatcher("table.jsp");
-	    response.setContentType("text/html; charset=UTF-8");
-		RequestDispatcher dispatcher = request.getRequestDispatcher("dashboard.jsp");
-		dispatcher.forward(request, response);
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		RequestDispatcher dispatcher = request.getRequestDispatcher("table.jsp");
+	    resp.setContentType("text/html; charset=UTF-8");
+		String url = req.getRequestURL().toString();
+	    if (url.contains("home")) {
+			req.getRequestDispatcher("views/web/index.jsp").forward(req, resp);
+		}
+		if (url.contains("userInfo")) {
+			req.getRequestDispatcher("views/web/user.jsp").forward(req, resp);
+		}
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
