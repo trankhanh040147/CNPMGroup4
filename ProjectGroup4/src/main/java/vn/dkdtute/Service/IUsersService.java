bf0280cef89f61f208b72268ecda799bebb65534
trankhanh040147 <78889860+trankhanh040147@users.noreply.github.com>
2022-12-02 00:19:42 +0700

    re-add Dang Nhap, Dang Xuat, Thay doi thong tin

---
+++ ProjectGroup4/src/main/java/vn/dkdtute/Service/IUsersService.java
@@ -0,0 +1,10 @@
+package vn.dkdtute.Service;
+
+import vn.dkdtute.Model.Users;
+
+public interface IUsersService {
+	Users findByID(String userid);
+	Users findByEmail(String email);
+	Users login(String email, String passwd);
+	void edit(Users user);
+}
