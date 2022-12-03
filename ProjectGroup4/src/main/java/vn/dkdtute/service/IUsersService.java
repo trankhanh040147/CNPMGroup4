c163a4466badfb9079e718efeb5e398e58642628
trankhanh040147 <78889860+trankhanh040147@users.noreply.github.com>
2022-12-04 00:46:16 +0700

    delete all

--- ProjectGroup4/src/main/java/vn/dkdtute/service/IUsersService.java
+++
@@ -1,8 +0,0 @@
-package vn.dkdtute.service;
-
-import vn.dkdtute.model.Users;
-
-public interface IUsersService {
-	Users findByID(String userid);
-	Users findByEmail(String email);
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-"# CNPMGroup4" 
