e168021f9c8b62c72ada7cf3eed8176037432a1c
AnNgoIT <ngothuaan2002@gmail.com>
2022-12-04 00:08:14 +0700

    Merge from all group member

---
+++ ProjectGroup4/src/main/java/vn/dkdtute/Dao/IUsersDao.java
@@ -0,0 +1,10 @@
+package vn.dkdtute.Dao;
+
+import vn.dkdtute.Model.Users;
+
+public interface IUsersDao {
+	Users findByID(String userid);
+	Users findByEmail(String email);
+	Users login(String email, String passwd);
+	void edit(Users user);
+}
