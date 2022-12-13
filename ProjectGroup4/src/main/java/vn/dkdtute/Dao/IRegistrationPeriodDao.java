d23f8bc2a46dfb7707ff278f504ba3034c9e0804
trankhanh040147 <78889860+trankhanh040147@users.noreply.github.com>
2022-12-13 13:18:25 +0700

    rm all

--- ProjectGroup4/src/main/java/vn/dkdtute/Dao/IRegistrationPeriodDao.java
+++
@@ -1,7 +0,0 @@
-package vn.dkdtute.Dao;
-
-import vn.dkdtute.Model.RegistrationPeriod;
-
-public interface IRegistrationPeriodDao {
-	void insert(RegistrationPeriod period);
-}
--- ProjectGroup4/src/main/java/vn/dkdtute/Service/ISubmitTopicService.java
+++
@@ -1,7 +0,0 @@
-package vn.dkdtute.Service;
-
-import vn.dkdtute.Model.SubmitTopic;
-
-public interface ISubmitTopicService {
-	void submit(SubmitTopic submit);
-}
